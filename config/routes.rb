27fa9a5ec54ac6e89321a5a2bb4fe88deceb5547
Bhanu Pratap <singh.bpratap86@gmail.com>
2020-12-14 14:18:22 +0530

    User signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      devise_for :users, :controllers => {registrations: 'api/v1/registrations'}
+    end
+  end
 end
